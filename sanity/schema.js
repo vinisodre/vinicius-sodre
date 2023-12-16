c4bbf13d0c1a26e41ec11066b74fd9cac8391036
Vinicius Sodré <vni217@gmail.com>
2023-12-16 16:42:13 -0300

    sanity conection

---
+++ sanity/schema.js
@@ -0,0 +1,7 @@
+import post from "./schemas/post"
+
+export const schema = {
+  types: [
+    post
+  ]
+}
---
+++ sanity/schemas/post.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'post',
+    type:'document',
+    title:'Post',
+    fields:  [
+        {
+            name: 'title',
+            type:'string',
+            title: 'Name'
+        }
+    ]
+}
